a971dd6d199921f22bc880ded4f1c28de21e40c5
ewara <ewara@git.com>
2018-11-24 00:51:07 +0800

    djf

--- src/main/java/com/m/hao/git/App.java
+++ src/main/java/com/m/hao/git/App.java
@@ -11,5 +11,6 @@ public class App
     	System.out.println("right...");
         System.out.println( "Hello World!" );
         System.out.println("df2s15");
+        System.out.println("hello jiaos");
     }
 }
